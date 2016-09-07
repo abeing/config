073844c67b68913d3485fca53f99938b1472abe9
Adam Miezianko <v-Adam.Miezianko@wizards.com>
2016-09-07 10:16:36 -0700

    Add WoW achievement focus picker.

---
+++ wow/focus.py
@@ -0,0 +1,14 @@
+# Find four achievements on which to focus.
+
+# Read every line in the input file. Put each line into an array unless it
+# starts with '#', '*' or '!'. Lines that start with '#' are comments to be
+# ignored. Lines that start with '*' are black-holed achievements to be
+# ignored. Lines that start with '!' are focused achievements and should be
+# added to the focus list immediately.
+#
+# Once every line is read, if the number of achievements in the focus list is
+# less than the desired number, select random achievements and add them to the
+# list until the desired number are focused.
+#
+# Output to standard out the list of focused achievements.
+
