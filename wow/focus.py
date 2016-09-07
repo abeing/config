aca1b8153d956b898ce5fd055feb7d8386e0c50a
Adam Miezianko <v-Adam.Miezianko@wizards.com>
2016-09-07 11:06:53 -0700

    Cleaned up version. Does not do selection without replacement yet.

--- wow/focus.py
+++ wow/focus.py
@@ -12,11 +12,46 @@
 #
 # Output to standard out the list of focused achievements.
 
+import random
 
-def read_datafile(data_filename, achievements):
+DESIRED_FOCUS = 4
+
+
+def is_ignored_achievement(achievement):
+    """True if this achievement is to be ignored."""
+    return achievement[0] == '#' or achievement[0] == '*'
+
+
+def is_focused_achievement(achievement):
+    """True if this achievement has been marked for focus."""
+    return achievement[0] == '!'
+
+
+def process_datafile(data_filename):
     """Read achievements from data file. Ignore disabled achievements and
     focus focued achievements."""
+    focus_achievements = []
+    other_achievements = []
     with open(data_filename, 'r') as datafile:
-        for line in datafile:
-            achievements.append(line)
+        for achievement in datafile:
+            achievement = achievement.rstrip()
+            if len(achievement) == 0:
+                continue
+            if is_focused_achievement(achievement):
+                focus_achievements.append(achievement)
+            elif not is_ignored_achievement(achievement):
+                other_achievements.append(achievement)
     datafile.close()
+    while len(focus_achievements) < DESIRED_FOCUS:
+        focus_achievements.append(random.choice(other_achievements))
+    return focus_achievements
+
+
+def main():
+    focus_achievements = process_datafile("aephos.data")
+    for achievement in focus_achievements:
+        print achievement
+
+
+if __name__ == "__main__":
+    main()
