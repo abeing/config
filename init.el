13db664b3d50a23e0a617b9cb1cee9e6bfbccf96
Adam Miezianko <adam.miezianko@gmail.com>
2022-02-11 21:42:48 -0800

    Moved to Linux.
    I moved my main environment from macOS to Linux. This is my first update to my
    emacs config catching all the changes from that move.

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
 
 (add-to-list 'initial-frame-alist '(fullscreen . fullheight))
 
-(set-face-attribute 'default nil :font "Go Mono" :height 140)
+(set-face-attribute 'default nil :font "Go Mono" :height 120)
 
 (setq custom-safe-themes t)
 
@@ -247,12 +247,69 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(awesome-tray-mode-line-active-color "#0031a9")
+ '(awesome-tray-mode-line-inactive-color "#d7d7d7")
+ '(exwm-floating-border-color "#888888")
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-themes-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-themes-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-themes-fringe-yellow))
  '(hl-sexp-background-color "#efebe9")
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#70480f")
+     ("TODO" . "#721045")
+     ("NEXT" . "#5317ac")
+     ("THEM" . "#8f0075")
+     ("PROG" . "#00538b")
+     ("OKAY" . "#30517f")
+     ("DONT" . "#315b00")
+     ("FAIL" . "#a60000")
+     ("BUG" . "#a60000")
+     ("DONE" . "#005e00")
+     ("NOTE" . "#863927")
+     ("KLUDGE" . "#813e00")
+     ("HACK" . "#813e00")
+     ("TEMP" . "#5f0000")
+     ("FIXME" . "#a0132f")
+     ("XXX+" . "#972500")
+     ("REVIEW" . "#005a5f")
+     ("DEPRECATED" . "#201f55")))
+ '(ibuffer-deletion-face 'modus-themes-mark-del)
+ '(ibuffer-filter-group-name-face 'modus-themes-pseudo-header)
+ '(ibuffer-marked-face 'modus-themes-mark-sel)
+ '(ibuffer-title-face 'default)
+ '(org-src-block-faces 'nil)
  '(package-selected-packages
-   '(modus-themes zenburn-theme which-key use-package tao-theme solarized-theme rg powerline poet-theme plan9-theme org-roam org-pomodoro nordless-theme nord-theme moe-theme markdown-mode magit leuven-theme gruvbox-theme gotham-theme flatui-theme evil elpher diminish cyberpunk-theme counsel color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized)))
+   '(bongo nov w3m modus-themes zenburn-theme which-key use-package tao-theme solarized-theme rg powerline poet-theme plan9-theme org-roam org-pomodoro nordless-theme nord-theme moe-theme markdown-mode magit leuven-theme gruvbox-theme gotham-theme flatui-theme evil elpher diminish cyberpunk-theme counsel color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized))
+ '(pdf-view-midnight-colors '("#000000" . "#f8f8f8"))
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#a60000")
+     (40 . "#721045")
+     (60 . "#8f0075")
+     (80 . "#972500")
+     (100 . "#813e00")
+     (120 . "#70480f")
+     (140 . "#5d3026")
+     (160 . "#184034")
+     (180 . "#005e00")
+     (200 . "#315b00")
+     (220 . "#005a5f")
+     (240 . "#30517f")
+     (260 . "#00538b")
+     (280 . "#093060")
+     (300 . "#0031a9")
+     (320 . "#2544bb")
+     (340 . "#0000c0")
+     (360 . "#5317ac")))
+ '(vc-annotate-very-old-color nil)
+ '(xterm-color-names
+   ["black" "#a60000" "#005e00" "#813e00" "#0031a9" "#721045" "#00538b" "gray65"])
+ '(xterm-color-names-bright
+   ["gray35" "#972500" "#315b00" "#70480f" "#2544bb" "#8f0075" "#30517f" "white"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(variable-pitch ((t (:family "Go")))))
