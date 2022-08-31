8f97cff3c653502a785e73a78fbbfc0a3f68bc67
Adam Miezianko <adam.miezianko@gmail.com>
2022-08-30 20:05:21 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,4 +6,8 @@ function bc-dl
     yt-dlp -o '%(artist)s - %(album)s - %(track_number)02d - %(track)s.mp3' $argv
 end
 
+function sdl
+    python -m spotdl "https://open.spotify.com/album/"$argv
+end
+
 fish_add_path ~/bin
