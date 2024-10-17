53db8a81e878b8bf15e6f8c39aee2291be868876
Adam Miezianko <adam.miezianko@gmail.com>
2024-10-16 18:57:47 -0700

    Add org-drill

--- emacs/init.el
+++ emacs/init.el
@@ -470,6 +470,11 @@
   (go-mode . (lambda () (setq tab-width 8)))
   )
 
+;;; --------------------[ Drill ]-----------------------------------------------
+
+(use-package org-drill
+  :ensure t)
+
 ;;; --------------------[ Proselint ]-------------------------------------------
 
 (use-package flymake-proselint
