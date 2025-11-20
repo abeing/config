ca9d53cb4a0f4dadb9a9176c38f66edb58044e41
Adam Miezianko <adam@theorylounge.org>
2025-11-20 09:59:20 -0800

    Add paragraph reformatting functionality
    Added new functions for cycling paragraph formatting between filled, unfilled,
    and sentence-fill states. Implemented am-repeat-next to manage repeated
    command sequences, and am-unfill-paragraph to inverse fill-paragraph. The new
    am-reformat-paragraph-or-region function handles both single paragraphs and
    regions, with special handling for org-mode elements. Bound the new
    functionality to M-q key. Also added am-in-prefixed-comment-p to detect
    comment contexts and prepared for future integration of fill-paragraph-semlf
    when available in Emacs.

--- emacs/init.el
+++ emacs/init.el
@@ -580,6 +580,93 @@
   (interactive)
   (goto-line (1+ (random (count-lines (point-min) (point-max))))))
 
+(defvar am-repeat-counter '()
+  "How often `am-repeat-next' was called in a row using the same command.
+   This is an alist of (cat count list) so we can use it for different
+   functions.")
+
+(defun am-unfill-paragraph ()
+  "Replace newline chars in current paragraph by single spaces.  This
+   command does the inverse of `fill-paragraph'."
+  (interactive)
+  (let ((fill-column most-positive-fixnum))
+    (fill-paragraph)))
+
+;; Since my version of Emacs doesn't have `fill-paragraph-semlf' yet, I can't
+;; use semantic linefeed filling.
+;; (defun am-fill-paragraph-semlf-long ()
+;;   (interactive)
+;;   (let ((fill-column most-positive-fixnum))
+;;     (fill-paragraph-semlf)))
+
+(defun am-repeat-next (category &optional element-list reset)
+  "Return the next element for CATEGORY.  Initialize with ELEMENT-LIST if
+   this is the first time."
+  (let* ((counter
+          (or (assoc category am-repeat-counter)
+              (progn
+                (push (list category -1 element-list)
+                      am-repeat-counter)
+                (assoc category am-repeat-counter)))))
+    (setf (elt (cdr counter) 0)
+          (mod
+           (if reset 0 (1+ (elt (cdr counter) 0)))
+           (length (elt (cdr counter) 1))))
+    (elt (elt (cdr counter) 1) (elt (cdr counter) 0))))
+
+(defun am-in-prefixed-comment-p ()
+  (or (member 'font-lock-comment-delimiter-face (face-at-point nil t))
+      (member 'font-lock-comment-face (face-at-point nil t))
+      (save-excursion
+        (beginning-of-line)
+        (comment-search-forward (line-end-position) t))))
+
+;; It might be nice to figure out what state we're in and then cycle to the
+;; next one if we're just working with a single paragraph.  In the meantime,
+;; just going by repeats is fine.
+;;
+;; TODO: Add `fill-paragraph-semlf' and `am-fill-paragraph-semlf-long' to the
+;; list passed to `am-repeat-next' when `fill-paragraph-semlf' is available in
+;; Emacs.
+(defun am-reformat-paragraph-or-region ()
+  "Cycles the paragraph between three states:
+   filled/unfilled/fill-sentences.  If a region is selected, handle all
+   paragraphs within that region."
+  (interactive)
+  (let ((func (am-repeat-next 'am-reformat-paragraph
+                              '(fill-paragraph am-unfill-paragraph)  ; Add
+                                                                     ; extra
+                                                                     ; fills
+                                                                     ; here
+                              (not (eq this-command last-command))))
+        (deactivate-mark nil))
+    (if (region-active-p)
+        (save-restriction
+          (save-excursion
+            (narrow-to-region (region-beginning) (region-end))
+            (goto-char (point-min))
+            (while (not (eobp))
+              (skip-syntax-forward " ")
+              (let ((elem (and (derived-mode-p 'org-mode)
+                               (org-element-context))))
+                (cond
+                 ((eq (org-element-type elem) 'headline)
+                  (org-forward-paragraph))
+                 ((member (org-element-type elem)
+                          '(src-block export-block headline property-drawer))
+                  (goto-char
+                   (org-element-end (org-element-context))))
+                 (t
+                  (funcall func)
+                  (if fill-forward-paragraph-function
+                      (funcall fill-forward-paragraph-function)
+                    (forward-paragraph))))))))
+      (save-excursion
+        (move-to-left-margin)
+        (funcall func)))))
+
+(keymap-global-set "M-q" #'am-reformat-paragraph-or-region)
+
 ;;; --------------------[ Macros ]----------------------------------------------
 
 (defalias 'tvdb
